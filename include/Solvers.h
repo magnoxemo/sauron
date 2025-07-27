82020e19d3386e638b67f632ff16ee18bde9cfbf
magnoxemo <walid.ahammed01@gmail.com>
2025-07-26 22:10:35 -0500

    adding the quad solver

--- include/Solvers.h
+++ include/Solvers.h
@@ -1,19 +1,14 @@
 
 #ifndef LIBMESH_TRACK_LENGTH_SOLVER_H
 #define LIBMESH_TRACK_LENGTH_SOLVER_H
-#define EPSILON 0.000001
 
 class Point;
 class Ray;
 
-class Solver()
-
-{
-public:
-
-Point triangleSolver(Ray &ray, std::vector <Point> &edges) const;
-
-Point quadSolver(Ray &ray, std::vector <Point> &edges) const ;
+class Solver(){
+    const double EPSILON = 0.000001;
+    template <typename T> triangleSolver(Ray &ray, std::vector <Point> &edges) const;
+    template <typename T> quadSolver(Ray &ray, std::vector <Point> &edges) const ;
 
 }
 #endif
--- src/Solver.cpp
+++ src/Solver.cpp
@@ -20,9 +20,27 @@ Solver::triangleSolver(Ray & ray, std::vector<Point>& edges) const {
 
     //if barycentric co-ordinate u<=0 or v<=0 or (v+u)>1 the ray wouldn't interset the triangle
     if ( (solution[1]+solution[2]>1) or  solution[1]<=EPSILON solution[2]<=EPSILON){
-        return -1
+        return -1 ;
     }
     else
         return solution;
 
+}
+
+template <typename T>
+Solver::quadSolver(Ray &ray, std::vector <Point> &edges) const{
+    // no need to solve independtly
+    // I will the triangleSolver twice in this case
+    // first part the quad into two triangle
+    std::vector<Point> triangle_1 (edges[0], edges[1], edges[2]);
+    auto solution_1 = triangelSolver(ray, triangle_1);
+
+    //only solve the other one if the first one is a miss
+    if (solution_1 !=-1){
+        std::vector<Point> tirangle_2 (edges[0], edges[3], edges[2]);
+        auto solution_2 = tiangleSolver(ray,triangle_2);
+        //if the solution doesn't exists triangleSolver will handle it anyway
+        return  solution_2;
+    }
+    return solution_1;
 }

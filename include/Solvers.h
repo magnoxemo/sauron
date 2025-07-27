57fbc9b544c7dfa92f42160cb1a09132eb45de66
magnoxemo <walid.ahammed01@gmail.com>
2025-07-27 15:45:25 -0500

    bug fix and typo

--- include/Solvers.h
+++ include/Solvers.h
@@ -6,7 +6,7 @@ class Point;
 class Ray;
 
 class Solver(){
-    const double EPSILON = 0.000001;
+    constexpr double EPSILON = 0.000001;
     template <typename T> triangleSolver(Ray &ray, std::vector <Point> &edges) const;
     template <typename T> quadSolver(Ray &ray, std::vector <Point> &edges) const ;
 
--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,46 +1,55 @@
 #include "Solver.h"
 #include "Vector.h"
+#include <optional>  // For optional return type
 
 
 //this method implements the Moller Trumbore algorithm
 template <typename T>
-Solver::triangleSolver(Ray & ray, std::vector<Point>& edges) const {
+std::optional<Vector3D> Solver::triangleSolver(Ray &ray, std::vector<Point>& edges) const {
 
-    const auto edge_1 = edges[1] - edges[0];
-    const auto edge_2 = edges[2] - edges[0];
-    const auto T = ray._direction - edges[0];
+    const auto edge1 = edges[1] - edges[0];
+    const auto edge2 = edges[2] - edges[0];
 
-    const auto Q = T.crossProduct(edge_1);
-    const auto P = D.crossProduct(edge_2);
+    const auto P = ray._direction.crossProduct(edge2);
+    const double det = P.dotProduct(edge1);
 
-    auto solution =  Vector3D(Q.dotProduct(edge_2),
-                              P.dotProduct(T),
-                              Q.dotProduct(ray._direction)
-                              )/P.dotProduct(edge_1);
+    if (std::fabs(det) < EPSILON)
+        return std::nullopt;
 
-    //if barycentric co-ordinate u<=0 or v<=0 or (v+u)>1 the ray wouldn't interset the triangle
-    if ( (solution[1]+solution[2]>1) or  solution[1]<=EPSILON solution[2]<=EPSILON){
-        return -1 ;
+    const double invDet = 1.0 / det;
+    const auto T = ray._origin - edges[0];
+
+    const double u = T.dotProduct(P) * invDet;
+    if (u < 0.0 || u > 1.0)
+        return std::nullopt;
+
+    const auto Q = T.crossProduct(edge1);
+    const double v = ray._direction.dotProduct(Q) * invDet;
+    if (v < 0.0 || (u + v) > 1.0)
+        return std::nullptr;
+
+    const double t = edge2.dotProduct(Q) * invDet;
+    if (t > EPSILON) {
+        return Vector3D(t, u, v);
     }
-    else
-        return solution;
 
+    return std::nullopt;
 }
 
 template <typename T>
-Solver::quadSolver(Ray &ray, std::vector <Point> &edges) const{
+std::optional<Vector3D> Solver::quadSolver(Ray &ray, std::vector<Point> &edges) const {
     // no need to solve independtly
     // I will the triangleSolver twice in this case
     // first part the quad into two triangle
-    std::vector<Point> triangle_1 (edges[0], edges[1], edges[2]);
-    auto solution_1 = triangelSolver(ray, triangle_1);
+    std::vector<Point> triangle1{edges[0], edges[1], edges[2]};
+    auto solution1 = triangleSolver<T>(ray, triangle1);
+
+    if (solution1.has_value())
+        return solution1;
 
     //only solve the other one if the first one is a miss
-    if (solution_1 !=-1){
-        std::vector<Point> tirangle_2 (edges[0], edges[3], edges[2]);
-        auto solution_2 = tiangleSolver(ray,triangle_2);
-        //if the solution doesn't exists triangleSolver will handle it anyway
-        return  solution_2;
-    }
-    return solution_1;
+    std::vector<Point> triangle2{edges[0], edges[2], edges[3]};
+    auto solution2 = triangleSolver<T>(ray, triangle2);
+    //if the solution doesn't exists triangleSolver will handle it anyway
+    return solution2;
 }

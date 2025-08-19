e045b04acdbc7b98c10d73a1aa65b926e729ee5e
magnoxemo <walid.ahammed01@gmail.com>
2025-08-19 05:13:42 -0500

    adding corner nodes determination.

---
+++ include/NumCornerNodes.h
@@ -0,0 +1,31 @@
+#ifndef SAURON_NUMCORNERNODES_H
+#define SAURON_NUMCORNERNODES_H
+
+#include "libmesh/elem.h"
+/*this is going to be a header only section where I will create a switch
+ system to evaluate how many corners are in a 3D libmesh element*/
+
+inline int number_of_corner_nodes_on_a_side(const libMesh::ElemType element_type) {
+    switch (element_type) {
+
+        case libMesh::TET4:
+            return 3;
+        case libMesh::TET10:
+            return 3;
+
+        case libMesh::HEX8:
+            return 4;
+        case libMesh::HEX20:
+            return 4;
+        case libMesh::HEX27:
+            return 4;
+
+        default:
+            //should I throw an error this case?
+            return -1;
+    }
+}
+
+
+
+#endif //SAURON_NUMSIDES_H

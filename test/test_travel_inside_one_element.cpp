708e0932f3d7ad1796d2cd20ae859f0a658b411b
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2025-10-21 22:19:57 -0500

    Refactor test_in_voxel_track

--- test/test_travel_inside_one_element.cpp
+++ test/test_travel_inside_one_element.cpp
@@ -18,15 +18,13 @@
 
 using namespace sauron;
 
-double actual_distance;
-double total_track = 0;
-double relative_error;
-unsigned int element_intercepted;
 
 
-void test_in_voxel_track(int argc, char* argv []){
+TEST(track_length_calculation, test_in_voxel_track_calculation) {
 
-    libMesh::LibMeshInit init(argc, argv);
+    double total_track = 0;
+
+    libMesh::LibMeshInit init(0, nullptr);
     libMesh::Mesh mesh(init.comm());
 
     libMesh::MeshTools::Generation::build_cube(mesh,24,18,30,0,10,0,10,0,10,libMesh::TET4);
@@ -34,25 +32,21 @@ void test_in_voxel_track(int argc, char* argv []){
 
     std::default_random_engine random_number_generator;
     std::uniform_real_distribution <double> distribution (0.01,0.02);
-
     Point starting_point = {distribution(random_number_generator), distribution(random_number_generator), distribution(random_number_generator)};
     Point destination =  {distribution(random_number_generator), distribution(random_number_generator), distribution(random_number_generator)} ;
-    actual_distance = get_distance (starting_point, destination);
+    
+    auto actual_distance = get_distance (starting_point, destination);
 
     MiddleEarth gondor(unstractured_mesh);
     auto [ids, values]  = gondor.parallelNazgulSolver(starting_point, destination);
 
-    for (auto value:values){
+    for (auto value:values)
         total_track+=value;
-    }
-
-    relative_error = (total_track - actual_distance) * 100 / actual_distance;
-    element_intercepted = ids.size();
-
-}
 
 
-TEST(track_length_calculation, test_in_voxel_track_calculation) {
+    auto relative_error = (total_track - actual_distance) * 100 / actual_distance;
+    auto element_intercepted = ids.size();
+
     EXPECT_NEAR(actual_distance, total_track, 1e-10);
     EXPECT_NEAR(relative_error, 0, 1e-10);
     EXPECT_EQ(element_intercepted,1);
@@ -61,7 +55,6 @@ TEST(track_length_calculation, test_in_voxel_track_calculation) {
 
 int main(int argc, char **argv) {
 
-    test_in_voxel_track(argc,argv);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
